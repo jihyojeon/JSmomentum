f4fb6c555bab11f7e70e0f681d84ef291f77bf7c
JIHYO-JEON <cori950920@gmail.com>
2021-07-12 17:43:31 +0900

    Add Log-In Function

--- app.js
+++ app.js
@@ -23,11 +23,29 @@
 
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault(); // Prevent refreshing
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    console.log('Hello', username);
+    localStorage.setItem(USERNAME_KEY, username); //Memorizing
+    paintGreetings(username); // call another function
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+  }
+  
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+  } else {
+    paintGreetings(savedUsername);
+  }
