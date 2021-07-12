637e83f66172b103fa5ba35e03b10196f1f4eb3d
JIHYO-JEON <cori950920@gmail.com>
2021-07-12 13:09:26 +0900

    Add input values

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+
+// const loginInput = document.querySelector("#login-form input");
+// const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick(){
+    console.log('Hello', loginInput.value);
+}
+
+loginButton.addEventListener("click",onLoginBtnClick);
