6c24394203d1735ef3c00d94bc7f5acbc80187fe
JIHYO-JEON <cori950920@gmail.com>
2021-07-12 13:12:14 +0900

    Add input values

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// Option 1 to find elements
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 
+// Option 2 to find elements
 // const loginInput = document.querySelector("#login-form input");
 // const loginButton = document.querySelector("#login-form button");
 
