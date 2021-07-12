1a8bacf17dc1c01db40502cddc6e939385ad8e73
JIHYO-JEON <cori950920@gmail.com>
2021-07-12 15:59:15 +0900

    Prevent refreshing

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
-// Option 1 to find elements
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+// // Option 1 to find elements
+// const loginForm = document.getElementById("login-form");
+// const loginInput = loginForm.querySelector("input");
+// const loginButton = loginForm.querySelector("button");
 
-// Option 2 to find elements
-// const loginInput = document.querySelector("#login-form input");
-// const loginButton = document.querySelector("#login-form button");
+// // Option 2 to find elements
+// // const loginInput = document.querySelector("#login-form input");
+// // const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick(){
+// function onLoginBtnClick(){
+//     const username = loginInput.value;
+//     if (username === ""){
+//         alert("Please write your name.");
+//     } else if (username.length > 15){
+//         alert("Your name is too long.")
+//     } else {
+//         console.log('Hello', username);
+//     }
+//     console.log('Hello', username);
+// }
+
+// loginButton.addEventListener("click",onLoginBtnClick);
+
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+function onLoginSubmit(event){
+    event.preventDefault(); // Prevent refreshing
     const username = loginInput.value;
-    // JS
-    // if (username === ""){
-    //     alert("Please write your name.");
-    // } else if (username.length > 15){
-    //     alert("Your name is too long.")
-    // } else {
-    //     console.log('Hello', username);
-    // }
     console.log('Hello', username);
 }
 
-loginButton.addEventListener("click",onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
