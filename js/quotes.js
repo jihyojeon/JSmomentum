c53bf67f30f6b521e7627aaff61e0cec43066c7e
JIHYO-JEON <cori950920@gmail.com>
2021-07-13 17:51:31 +0900

    add todo

--- js/quotes.js
+++ js/quotes.js
@@ -44,7 +44,7 @@ const quotes = [
   
   const quote = document.querySelector("#quote span:first-child");
   const author = document.querySelector("#quote span:last-child");
-  const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)]; // 0 ~ length-1
   
   quote.innerText = todaysQuote.quote;
   author.innerText = todaysQuote.author;
